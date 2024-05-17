c0ee7444b39e4c23906d39ef02c2e65e42a0fa89
Wassim SAMAD <wass08@gmail.com>
2024-05-17 17:29:50 +0900

    Update Interface.jsx

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   FormControl,
   FormControlLabel,
   FormLabel,
@@ -9,10 +10,12 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 import { useConfigurator } from "../contexts/Configurator";
 export const Interface = () => {
   const { tableWidth, setTableWidth, legs, setLegs, legsColor, setLegsColor } =
     useConfigurator();
+  const [opened, setOpened] = useState(window.innerWidth > 1024);
 
   return (
     <Box
@@ -26,69 +29,88 @@ export const Interface = () => {
       p={3}
     >
       <Stack spacing={3}>
-        <Typography variant="caption">Table Configurator</Typography>
-        <Box className="glass" p={3}>
-          <FormControl>
-            <FormLabel>Table width</FormLabel>
-            <Slider
-              sx={{
-                width: "200px",
-              }}
-              min={50}
-              max={200}
-              value={tableWidth}
-              onChange={(e) => setTableWidth(e.target.value)}
-              valueLabelDisplay="auto"
-            />
-          </FormControl>
-        </Box>
-        <Box className="glass" p={3}>
-          <FormControl>
-            <FormLabel>Legs Layout</FormLabel>
-            <RadioGroup
-              value={legs}
-              onChange={(e) => setLegs(parseInt(e.target.value))}
-            >
-              <FormControlLabel
-                value={0}
-                control={<Radio />}
-                label="Standard"
-              />
-              <FormControlLabel value={1} control={<Radio />} label="Solid" />
-              <FormControlLabel value={2} control={<Radio />} label="Design" />
-            </RadioGroup>
-          </FormControl>
-        </Box>
-        <Box className="glass" p={3}>
-          <FormControl>
-            <FormLabel>Legs Color</FormLabel>
-            <RadioGroup
-              value={legsColor}
-              onChange={(e) => setLegsColor(e.target.value)}
-            >
-              <FormControlLabel
-                value={"#777777"}
-                control={<Radio />}
-                label="Black"
-              />
-              <FormControlLabel
-                value={"#ECECEC"}
-                control={<Radio />}
-                label="Chrome"
-              />
-              <FormControlLabel
-                value={"#C9BD71"}
-                control={<Radio />}
-                label="Gold"
-              />
-              <FormControlLabel
-                value={"#C9A3B9"}
-                control={<Radio />}
-                label="Pink Gold"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Box>
+        <Button
+          onClick={() => setOpened((opened) => !opened)}
+          style={{
+            justifyContent: "flex-end",
+          }}
+        >
+          <Typography variant="caption">{opened ? "Close" : "Open"}</Typography>
+        </Button>
+        {opened && (
+          <>
+            <Box className="glass" p={3}>
+              <FormControl>
+                <FormLabel>Table width</FormLabel>
+                <Slider
+                  sx={{
+                    width: "200px",
+                  }}
+                  min={50}
+                  max={200}
+                  value={tableWidth}
+                  onChange={(e) => setTableWidth(e.target.value)}
+                  valueLabelDisplay="auto"
+                />
+              </FormControl>
+            </Box>
+            <Box className="glass" p={3}>
+              <FormControl>
+                <FormLabel>Legs Layout</FormLabel>
+                <RadioGroup
+                  value={legs}
+                  onChange={(e) => setLegs(parseInt(e.target.value))}
+                >
+                  <FormControlLabel
+                    value={0}
+                    control={<Radio />}
+                    label="Standard"
+                  />
+                  <FormControlLabel
+                    value={1}
+                    control={<Radio />}
+                    label="Solid"
+                  />
+                  <FormControlLabel
+                    value={2}
+                    control={<Radio />}
+                    label="Design"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Box>
+            <Box className="glass" p={3}>
+              <FormControl>
+                <FormLabel>Legs Color</FormLabel>
+                <RadioGroup
+                  value={legsColor}
+                  onChange={(e) => setLegsColor(e.target.value)}
+                >
+                  <FormControlLabel
+                    value={"#777777"}
+                    control={<Radio />}
+                    label="Black"
+                  />
+                  <FormControlLabel
+                    value={"#ECECEC"}
+                    control={<Radio />}
+                    label="Chrome"
+                  />
+                  <FormControlLabel
+                    value={"#C9BD71"}
+                    control={<Radio />}
+                    label="Gold"
+                  />
+                  <FormControlLabel
+                    value={"#C9A3B9"}
+                    control={<Radio />}
+                    label="Pink Gold"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Box>
+          </>
+        )}
       </Stack>
     </Box>
   );
